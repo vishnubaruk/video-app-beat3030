76646ab637b54926f8726946c256d651995c42a2
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2020-09-28 21:41:25 +0300

    ✨ Toolbar in activity_main.xml done ✨ Added resources for buttons and prototype functionality including icon change when changing app state 🐛 Fixed bugs related to toolbar by changing it with ConstraintLayout

--- app/src/main/java/com/example/tiktok_analog/MainActivity.kt
+++ app/src/main/java/com/example/tiktok_analog/MainActivity.kt
@@ -1,12 +1,76 @@
 package com.example.tiktok_analog
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var isMenuOpened = false
+    var isFilterOpened = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openMenuButton.setOnClickListener {
+            openMenu()
+        }
+
+        openFilterButton.setOnClickListener {
+            openFilter()
+        }
+
+        closeMenuButton.setOnClickListener{
+            closeMenu()
+        }
+
+        closeFilterButton.setOnClickListener{
+            closeFilter()
+        }
+    }
+
+    private fun openMenu() {
+        Toast.makeText(applicationContext, "Menu Opened!", Toast.LENGTH_SHORT).show()
+        closeFilter()
+
+        openMenuButton.visibility = View.GONE
+        closeMenuButton.visibility = View.VISIBLE
+
+        isMenuOpened = true
+
+        sectionTitleText.text = "Меню"
+    }
+
+    private fun closeMenu() {
+        Toast.makeText(applicationContext, "Menu Closed!", Toast.LENGTH_SHORT).show()
+
+        openMenuButton.visibility = View.VISIBLE
+        closeMenuButton.visibility = View.GONE
+
+        isMenuOpened = false
+
+        sectionTitleText.text = "Главная"
+    }
+
+    private fun openFilter() {
+        Toast.makeText(applicationContext, "Filter Opened!", Toast.LENGTH_SHORT).show()
+        closeMenu()
+
+        openFilterButton.visibility = View.GONE
+        closeFilterButton.visibility = View.VISIBLE
+
+        isFilterOpened = true
+    }
+
+    private fun closeFilter() {
+        Toast.makeText(applicationContext, "Filter Closed!", Toast.LENGTH_SHORT).show()
+
+        openFilterButton.visibility = View.VISIBLE
+        closeFilterButton.visibility = View.GONE
+
+        isFilterOpened = false
     }
 }
