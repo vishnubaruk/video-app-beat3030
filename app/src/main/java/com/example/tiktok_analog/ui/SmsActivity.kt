51efa9690cd060d9810f3c6af9a44df9ad3adeea
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-02-19 15:26:31 +0300

    ✨ Implemented user data reading in SmsActivity

--- app/src/main/java/com/example/tiktok_analog/ui/SmsActivity.kt
+++ app/src/main/java/com/example/tiktok_analog/ui/SmsActivity.kt
@@ -1,23 +1,37 @@
 package com.example.tiktok_analog.ui
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.tiktok_analog.ui.main.MainActivity
 import com.example.tiktok_analog.R
+import com.example.tiktok_analog.data.model.User
 import com.example.tiktok_analog.util.GenericTextWatcher
 import kotlinx.android.synthetic.main.activity_sms.*
+import kotlinx.android.synthetic.main.menu.*
+import org.json.JSONObject
 
 class SmsActivity : AppCompatActivity() {
+
+    private lateinit var userData: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sms)
+
+        openFileInput("userData").use {
+            userData = User.newUser(JSONObject(it.readBytes().toString(Charsets.UTF_8)))
+        }
 
         val rootView = findViewById<View>(android.R.id.content).rootView
 
@@ -65,7 +79,32 @@ class SmsActivity : AppCompatActivity() {
         et4.addTextChangedListener(GenericTextWatcher(rootView, et4))
 
         sendSms.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            val sendSmsQueue = Volley.newRequestQueue(this)
+
+            val url =
+                "https://kepler88d.pythonanywhere.com/smsConfirmation?" +
+                        "phone=${userData.phone}&email=${userData.email}&code=${et1.text}${et2.text}${et3.text}${et4.text}"
+
+            val registerRequest = StringRequest(Request.Method.GET, url, { response ->
+                run {
+                    val jsonResponse = JSONObject(response)
+
+                    if (jsonResponse.getBoolean("ok")) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        AlertDialog.Builder(this).setTitle("Неверный код из смс")
+                            .setMessage("Попробуйте еще раз")
+                            .setPositiveButton("Хорошо") { dialog, _ ->
+                                dialog.cancel()
+                                onBackPressed()
+                            }.create().show()
+                    }
+                }
+            }, {
+                Log.e("ERROR", "Error at sign in : " + it.message)
+            })
+
+            sendSmsQueue.add(registerRequest)
         }
     }
 }
