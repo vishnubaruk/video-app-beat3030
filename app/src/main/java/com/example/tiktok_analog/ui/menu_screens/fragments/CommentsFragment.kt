112633bb2710bfec9078f3781dd616ac7efdcf81
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-03-11 02:55:31 +0300

    🔨 Created prototypes of CommentsFragment and ProfileFragment.kt

---
+++ app/src/main/java/com/example/tiktok_analog/ui/menu_screens/fragments/CommentsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.tiktok_analog.ui.menu_screens.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tiktok_analog.R
+import kotlinx.android.synthetic.main.bottom_sheet.view.*
+
+class CommentsFragment : Fragment(R.layout.fragment_comments) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/tiktok_analog/ui/menu_screens/fragments/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.example.tiktok_analog.ui.menu_screens.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tiktok_analog.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
