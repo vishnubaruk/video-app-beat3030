fc9b73fd0503f246c25317ef22b1c6bc89608026
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-05-27 21:57:31 +0300

    🔨 AppConfig.adFrequency property

--- app/src/main/java/com/example/tiktok_analog/util/dataclasses/AppConfig.kt
+++ app/src/main/java/com/example/tiktok_analog/util/dataclasses/AppConfig.kt
@@ -4,4 +4,4 @@ import kotlinx.serialization.*
 import com.example.tiktok_analog.util.enums.SortType
 
 @Serializable
-data class AppConfig(val sortType: SortType = SortType.ByPopularity)
+data class AppConfig(val sortType: SortType = SortType.ByPopularity, val adFrequency: Int = 5)
