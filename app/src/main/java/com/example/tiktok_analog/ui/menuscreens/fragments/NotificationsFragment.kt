519c8770272fc0031d85b869a03a66fb2d2503c3
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-06-07 22:04:13 +0300

    ✨ Fragment prototypes for screens: AddVideoFragment.kt, BroadcastFragment.kt, FavouriteFragment.kt, NotificationsFragment.kt

---
+++ app/src/main/java/com/example/tiktok_analog/ui/menuscreens/fragments/NotificationsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.tiktok_analog.ui.menuscreens.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tiktok_analog.R
+
+class NotificationsFragment(contentLayoutId: Int) : Fragment(R.layout.notifications) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
