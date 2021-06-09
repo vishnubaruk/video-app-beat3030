62b534d402fd83a52fd5d53e170f9b3ee0bfaa7e
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-06-09 23:20:44 +0300

    🔨 NotificationsFragment functionality (duplicated from NotificationsActivity)

--- app/src/main/java/com/example/tiktok_analog/ui/menuscreens/fragments/NotificationsFragment.kt
+++ app/src/main/java/com/example/tiktok_analog/ui/menuscreens/fragments/NotificationsFragment.kt
@@ -4,15 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.tiktok_analog.R
+import com.example.tiktok_analog.databinding.NotificationsBinding
 
-class NotificationsFragment(contentLayoutId: Int) : Fragment(R.layout.notifications) {
+class NotificationsFragment : Fragment(R.layout.notifications) {
+    private var _binding: NotificationsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = NotificationsBinding.inflate(inflater, container, false)
+
+        binding.backArrowButton.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+
+        binding.notificationsSwipeRefresh.setOnRefreshListener {
+            binding.notificationsSwipeRefresh.isRefreshing = false
+            Toast.makeText(
+                requireContext(),
+                "Notifications Updated", Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
