6592cdb9b8a8d526580b7da586a5fea80e5976f5
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-06-29 22:21:07 +0300

    🔨 NotificationsFragment.addNotificationView and .updateNotifications

--- app/src/main/java/com/example/tiktok_analog/ui/legacy/NotificationsActivity.kt
+++ app/src/main/java/com/example/tiktok_analog/ui/legacy/NotificationsActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiktok_analog.R
-import kotlinx.android.synthetic.main.notifications.*
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 class NotificationsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.notifications)
+        setContentView(R.layout.fragment_notifications)
 
         backArrowButton.setOnClickListener {
             onBackPressed()
--- app/src/main/java/com/example/tiktok_analog/ui/menuscreens/fragments/NotificationsFragment.kt
+++ app/src/main/java/com/example/tiktok_analog/ui/menuscreens/fragments/NotificationsFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.tiktok_analog.R
-import com.example.tiktok_analog.databinding.NotificationsBinding
+import com.example.tiktok_analog.databinding.ItemNotificationBinding
+import com.example.tiktok_analog.databinding.FragmentNotificationsBinding
+import com.example.tiktok_analog.ui.OpenVideoActivity
+import com.example.tiktok_analog.util.RequestWorker
 
-class NotificationsFragment : Fragment(R.layout.notifications) {
-    private var _binding: NotificationsBinding? = null
+class NotificationsFragment : Fragment(R.layout.fragment_notifications) {
+    private var _binding: FragmentNotificationsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -18,7 +22,7 @@ class NotificationsFragment : Fragment(R.layout.notifications) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = NotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
 
         binding.backArrowButton.setOnClickListener {
             requireActivity().onBackPressed()
@@ -35,8 +39,59 @@ class NotificationsFragment : Fragment(R.layout.notifications) {
         return binding.root
     }
 
+    private fun addNotificationView(text: String, type: NotificationType, creationTime: Float) {
+        val viewBinding = ItemNotificationBinding.inflate(
+            layoutInflater,
+            binding.notificationLayout,
+            true
+        )
+        viewBinding.text.text = text
+        viewBinding.time.text = creationTime.toString()
+
+        viewBinding.icon.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireActivity(),
+                when (type) {
+                    NotificationType.RateVideo -> R.drawable.ic_like
+                    NotificationType.Subscribe -> R.drawable.ic_favorite
+                    NotificationType.Broadcast -> R.drawable.ic_starttranslation
+                    NotificationType.RateComment -> R.drawable.ic_comment
+                }
+            )
+        )
+    }
+
+    private fun updateNotifications() {
+        binding.notificationLayout.removeAllViews()
+
+        RequestWorker.getNotifications(
+            (requireActivity() as OpenVideoActivity).userData.userId.toInt()
+        ) {
+            for (index in 0 until it.length()) {
+                val jsonObject = it.getJSONObject(index)
+                addNotificationView(
+                    text = jsonObject.getString("text"),
+                    type = when (jsonObject.getInt("notificationType")) {
+                        0 -> NotificationType.RateVideo
+                        1 -> NotificationType.Subscribe
+                        2 -> NotificationType.Broadcast
+                        else -> NotificationType.RateComment
+                    },
+                    creationTime = jsonObject.getDouble("creationTime").toFloat()
+                )
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+private enum class NotificationType {
+    RateVideo,
+    Subscribe,
+    Broadcast,
+    RateComment
 }
